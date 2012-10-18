76b58f9059cf9d27fb50c01c3c6dba7bc93f72f3
Richard Pupnewfster <pupnewfster@gmail.com>
2012-10-18 16:45:36 -0400

    updated to create files needed in future

--- CCEconomy/src/com/github/CorporateCraft/cceconomy/Initialization.java
+++ CCEconomy/src/com/github/CorporateCraft/cceconomy/Initialization.java
@@ -7,18 +7,45 @@ public class Initialization
 {
 	public static void InitiateFiles()
 	{
-		File f = new File("plugins/CCEconomy/moneytracker.txt");
+		File f1 = new File("plugins/CCEconomy/moneytracker.txt");
+		File f2 = new File("plugins/CCEconomy/sellprices.txt");
+		File f3 = new File("plugins/CCEconomy/buyprices.txt");
+		File f4 = new File("plugins/CCEconomy/itemids.txt");
 		File d = new File("plugins/CCEconomy");
 		if(!d.exists())
 		{
 			boolean success = d.mkdir();
 			if (!success){}
 		}
-		if(!f.exists())
+		if(!f1.exists())
 		{
 			try
 			{
-				f.createNewFile();
+				f1.createNewFile();
+			}
+			catch (IOException e){}
+		}
+		if(!f2.exists())
+		{
+			try
+			{
+				f2.createNewFile();
+			}
+			catch (IOException e){}
+		}
+		if(!f3.exists())
+		{
+			try
+			{
+				f3.createNewFile();
+			}
+			catch (IOException e){}
+		}
+		if(!f4.exists())
+		{
+			try
+			{
+				f4.createNewFile();
 			}
 			catch (IOException e){}
 		}
